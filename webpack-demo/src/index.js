3f8e5433bb219d9d01993ec90bcee5d57a1867e1
Amit Mishra <amit.aakash@gmail.com>
2019-10-20 22:23:44 +0530

    Asset management started

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,9 +1,13 @@
 import _ from 'lodash';
+// insertion of css with webpack css loader and style loader
+import './style.css';
 
 function component () {
     const element = document.createElement('div');
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['hello', 'webpack'], ' ');
+    //element.classList.add('hello');
+    element.className = 'hello'; 
 
     return element;
 }
@@ -11,3 +15,5 @@ function component () {
 document.body.appendChild(component());
 
 // command is npx webpack --config webpack.config.js
+
+// npm run build
